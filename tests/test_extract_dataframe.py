939959ee60b165b37446347c10f42f81b7eb91a1
sidoine-dako <audedako@gmail.com>
2022-08-09 19:02:26 +0100

    Update test_extract_dataframe.py

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -57,7 +57,11 @@ class TestTweetDfExtractor(unittest.TestCase):
         )
 
     def test_find_full_text(self):
-        text = <provide a list of the first five full texts>
+        text = ['Extra random image (I):\n\nLets focus in one very specific zone of the western coast -&gt; Longjing District, Taichung #City, #Taiwan \n \n#Copernicus #Sentinel2 🛰️ 2022-08-03 \nFull Size -&gt; https://t.co/39IOoqJZR9 🧐 https://t.co/rdf21paD5P',
+ "#China's media explains the military reasons for each area of the drills in the #Taiwan Strait\n\nRead the labels in the pictures ⬇️ Via CGTN https://t.co/0J4ilou4iv",
+ "China even cut off communication, they don't anwer phonecalls from the US. But here clown @ZelenskyyUa enters the stage to ask #XiJinping to change Putin's mind.",
+ "Putin to #XiJinping : I told you my friend, Taiwan will be a vassal state, including nukes, much like the Ukrainian model. I warned you... But it took Pelosi to open China's eyes.",
+ 'I’m sorry, I thought Taiwan was an independent country because it had its own government, currency, military, travel documents, Olympic team etc. while the PRC was a gangster regime run by a bunch of communist bandits. I apologize for the confusion. #taiwan https://t.co/j4rM8cDdD8']
 
         self.assertEqual(self.df.find_full_text(), text)
 
@@ -65,26 +69,26 @@ class TestTweetDfExtractor(unittest.TestCase):
         self.assertEqual(
             self.df.find_sentiments(self.df.find_full_text()),
             (
-                <provide a list of the first five sentiment values>,
-                <provide a list of the first five polarity values>,
+                [-0.030000000000000006, -0.1, 0.0, 0.1, -6.938893903907228e-18],
+                [0.2625, 0.1, 0.0, 0.35, 0.55625],
             ),
         )
 
 
     def test_find_screen_name(self):
-        name = <provide a list of the first five screen names>
+        name = ['i_ameztoy', 'ZIisq', 'Fin21Free', 'Fin21Free', 'VizziniDolores']
         self.assertEqual(self.df.find_screen_name(), name)
 
     def test_find_followers_count(self):
-        f_count = <provide a list of the first five follower counts>
+        f_count = [20497, 65, 85, 85, 910]
         self.assertEqual(self.df.find_followers_count(), f_count)
 
     def test_find_friends_count(self):
-        friends_count = <provide a list of the first five friend's counts>
+        friends_count = [2621, 272, 392, 392, 2608]
         self.assertEqual(self.df.find_friends_count(), friends_count)
 
     def test_find_is_sensitive(self):
-        self.assertEqual(self.df.is_sensitive(), <provide a list of the first five is_sensitive values>)
+        self.assertEqual(self.df.is_sensitive(), ['', '', '', '', ''])
 
 
     # def test_find_hashtags(self):

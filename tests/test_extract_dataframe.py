05bc135aa083a3d59bb413685857a4ff738acb7d
sidoine-dako <audedako@gmail.com>
2022-08-10 11:12:57 +0100

    Update test_extract_dataframe.py

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -69,7 +69,12 @@ class TestTweetDfExtractor(unittest.TestCase):
         self.assertEqual(
             self.df.find_sentiments(self.df.find_full_text()),
             (
-                ([-0.030000000000000006, -0.1, 0.0, 0.1, -6.938893903907228e-18],[0.2625, 0.1, 0.0, 0.35, 0.55625]),
+                ([Sentiment(polarity=-0.030000000000000006, subjectivity=0.2625),
+                    Sentiment(polarity=-0.1, subjectivity=0.1),
+                    Sentiment(polarity=0.0, subjectivity=0.0),
+                    Sentiment(polarity=0.1, subjectivity=0.35),
+                    Sentiment(polarity=-6.938893903907228e-18, subjectivity=0.55625)]
+                )
                 [-0.030000000000000006, -0.1, 0.0, 0.1, -6.938893903907228e-18],
                 [0.2625, 0.1, 0.0, 0.35, 0.55625],
             ),
